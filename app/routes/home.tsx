c653a5d01fc3f461c441efd6a3bdc695a95082ad
Dago <tocarralero@gmail.com>
2026-01-18 22:40:04 -0500

    add delta test to see how db works

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -6,16 +6,17 @@ export function meta({}: Route.MetaArgs) {
 }
 
 export async function loader({ context }: Route.LoaderArgs) {
+    const start = Date.now();
     const sql = dbContext.get(context);
     const r = await sql`select * from users;`;
-    return { r };
+    return { r, delta: Date.now() - start };
 }
 
 export default function Home({ loaderData }: Route.ComponentProps) {
     return (
         <div>
             <pre>
-                <code>{JSON.stringify(loaderData.r, null, 2)}</code>
+                <code>{JSON.stringify({ data: loaderData.r, delta: loaderData.delta + " ms" }, null, 2)}</code>
             </pre>
         </div>
     );
